7325de2ab27ac1d488d89ea96514095b8064c3cc
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-11-02 20:20:27 +0900

    [FEAT] 댓글 스켈레톤 코드 작성

---
+++ src/main/java/com/grinder/domain/comment/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.grinder.domain.comment.service;
+
+import com.grinder.domain.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+}
