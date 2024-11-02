407a8e381ec0981b41b696a8701511b813d04e87
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-11-02 10:29:15 +0900

    [FEAT] 피드 관련 스켈레톤 코드 작성

---
+++ src/main/java/com/grinder/domain/feed/entity/Feed.java
@@ -0,0 +1,48 @@
+package com.grinder.domain.feed.entity;
+
+import com.grinder.common.entity.BaseDateEntity;
+import com.grinder.domain.cafe.entity.Cafe;
+import com.grinder.domain.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(indexes = {
+        @Index(name = "idx_cafe_id", columnList = "cafe_id"),
+        @Index(name = "idx_visible_updatedAt_feedId", columnList = "is_visible, updated_at, feed_id")
+})
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Feed extends BaseDateEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Cafe cafe;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    @Column(nullable = false)
+    private Boolean isVisible;
+
+    @Column(name = "grade")
+    private Integer grade;
+
+    @Column(name = "rank")
+    private Integer rank;
+
+}
---
+++ src/main/java/com/grinder/domain/feed/repository/FeedRepository.java
@@ -0,0 +1,9 @@
+package com.grinder.domain.feed.repository;
+
+import com.grinder.domain.feed.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedRepository extends JpaRepository<Feed, Long> {
+}
