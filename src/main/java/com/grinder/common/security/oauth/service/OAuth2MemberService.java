4342e388ee12a4b0b2ec1e1661587714266a2751
sunghyun <shlim0287@naver.com>
2024-11-09 10:13:20 +0900

    [REFACTOR] 비지니스 레이어의 책임을 가지게 분리.

--- src/main/java/com/grinder/common/security/oauth/service/OAuth2MemberService.java
+++ src/main/java/com/grinder/common/security/oauth/service/OAuth2MemberService.java
@@ -1,12 +1,12 @@
 package com.grinder.common.security.oauth.service;
 
 import com.grinder.common.security.oauth.model.OAuth2MemberDetails;
-import com.grinder.common.security.oauth.model.response.GoogleResponse;
-import com.grinder.common.security.oauth.model.response.KakaoResponse;
-import com.grinder.common.security.oauth.model.response.NaverResponse;
+import com.grinder.common.security.oauth.model.implement.MemberManager;
+import com.grinder.common.security.oauth.model.implement.OAuth2ResponseConverter;
 import com.grinder.common.security.oauth.model.response.OAuth2Response;
-import com.grinder.domain.member.model.TierType;
-import lombok.extern.slf4j.Slf4j;
+import com.grinder.domain.member.entity.MemberEntity;
+import com.grinder.domain.member.model.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -14,35 +14,16 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
+@RequiredArgsConstructor
 public class OAuth2MemberService extends DefaultOAuth2UserService {
+    private final OAuth2ResponseConverter oAuth2ResponseConverter;
+    private final MemberManager memberManager;
+
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException{
-
+    public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(request);
-
-        // 소셜 로그인 provider 판단
-        String registrationId = request.getClientRegistration().getRegistrationId();
-        OAuth2Response oAuth2Response = null;
-
-        if(registrationId.equals("naver")){
-
-            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
-
-        } else if (registrationId.equals("google")) {
-
-            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
-
-        } else if (registrationId.equals("kakao")) {
-
-            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
-
-        } else {
-            return null;
-        }
-
-        TierType type = TierType.SILVER; // 디폴트 티어
-
-        return new OAuth2MemberDetails(oAuth2Response,type);
+        OAuth2Response oAuth2Response = oAuth2ResponseConverter.convert(request, oAuth2User);
+        Member member = memberManager.findOrCreate(oAuth2Response);
+        return new OAuth2MemberDetails(oAuth2Response, member.getTier());
     }
 }
