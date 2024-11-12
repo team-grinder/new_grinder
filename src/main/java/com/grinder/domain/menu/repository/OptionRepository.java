2366d650ec122b61dc92243d8bee995928df7984
yohanKim <yh.kim@iea.co.kr>
2024-11-12 19:25:55 +0900

    [FEAT] 메뉴 및 옵션 스켈레톤 코드 작성

--- src/main/java/com/grinder/domain/menu/repository/OptionRepository.java
+++ src/main/java/com/grinder/domain/menu/repository/OptionRepository.java
@@ -1,9 +1,9 @@
 package com.grinder.domain.menu.repository;
 
-import com.grinder.domain.menu.entity.Option;
+import com.grinder.domain.menu.entity.OptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OptionRepository extends JpaRepository<Option, Long> {
+public interface OptionRepository extends JpaRepository<OptionEntity, Long> {
 }
