0e10df6d3798bc22bd9988207308008068aeeeaa
sunghyun <shlim0287@naver.com>
2024-12-02 10:47:06 +0900

    [CHORE] 네이밍 수정.

---
+++ src/main/java/com/grinder/domain/tabling/repository/TableCapacityRepository.java
@@ -0,0 +1,9 @@
+package com.grinder.domain.tabling.repository;
+
+import com.grinder.domain.tabling.entity.TableCapacityEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableCapacityRepository extends JpaRepository<TableCapacityEntity, Long> {
+    Optional<TableCapacityEntity> findByCafeId(Long cafeId);
+}
