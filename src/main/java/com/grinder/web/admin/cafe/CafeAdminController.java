5b93d7a775feff26f665db0a919fa0b13cc5aecb
sunghyun <shlim0287@naver.com>
2025-01-17 19:17:02 +0900

    [FEAT] 테이블링 관리자(추후 티어별 수정 고려) 관련 로직 추가.

--- src/main/java/com/grinder/web/admin/cafe/CafeAdminController.java
+++ src/main/java/com/grinder/web/admin/cafe/CafeAdminController.java
@@ -1,11 +1,30 @@
 package com.grinder.web.admin.cafe;
 
+import com.grinder.domain.cafe.model.Cafe;
+import com.grinder.domain.cafe.model.CafeCreate;
 import com.grinder.domain.cafe.service.CafeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/admin")
 public class CafeAdminController {
     private final CafeService cafeService;
+
+    @PostMapping("/cafe/create")
+    public ResponseEntity<Cafe> createCafe(@RequestBody CafeCreate request) {
+        Cafe cafe = cafeService.createCafe(
+                request.getName(),
+                request.getAddress(),
+                request.getDescription(),
+                request.getTel(),
+                request.getBusinessNumber()
+        );
+        return ResponseEntity.ok(cafe);
+    }
 }
